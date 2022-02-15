37622bde867507f1969e90513e045de0a6fd3d98
Austin <12504656+Austin-Scott@users.noreply.github.com>
2022-02-14 20:24:46 -0600

    Add more macros

--- my-macros.ts
+++ my-macros.ts
@@ -15,6 +15,26 @@ const macroExtensions = {
     // Type "-SNAPSHOT"
     "00002": newMacro()
         .typeAlphanumeric('-SNAPSHOT'),
+    // Type "MCOMSR-"
+    "00003": newMacro()
+        .typeAlphanumeric('MCOMSR-'),
+    // Trigger vim macro
+    "00004": newMacro()
+        .withShift(newMacro().typeAlphanumeric('2'))
+        .typeAlphanumeric('q'),
+    // Type low security test account password
+    "00005": newMacro()
+        .withShift(newMacro().typeAlphanumeric('t'))
+        .typeAlphanumeric('est')
+        .withShift(newMacro().typeAlphanumeric('p'))
+        .typeAlphanumeric('a55w0rd')
+        .withShift(newMacro().typeAlphanumeric('1')),
+    // Goto next conflict marker
+    "00006": newMacro()
+        .typeAlphanumeric('/')
+        .withShift(newMacro().typeAlphanumeric('...'))
+        .typeAlphanumeric('\n')
+
 }
 
 processAll(macroExtensions, SOURCE_DIR)
