0adedb29f1e83a848fec51b334eae27fca2d9612
Austin <12504656+Austin-Scott@users.noreply.github.com>
2022-02-14 18:43:45 -0600

    Add support for parens)

--- macros.ts
+++ macros.ts
@@ -84,6 +84,12 @@ export const newMacro: (expectedReplacementCount?: number) => MacroBuilder = (er
                 else if (char === '"') {
                     self.withShift(newMacro().tapKey("X_QUOTE"))
                 }
+                else if (char === '(') {
+                    self.withShift(newMacro().tapKey("X_8"))
+                }
+                else if (char === ')') {
+                    self.withShift(newMacro().tapKey("X_9"))
+                }
                 else {
                     throw Error("Unsupported char in typeAlphanumeric: " + char + ", use typeRaw instead?")
                 }
